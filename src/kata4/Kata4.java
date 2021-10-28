82a1de509423c7e6f250d5024c8ed577de30b5a5
Lab-DIS <Lab-DIS@10.0.2.15>
2021-10-28 09:17:58 +0100

    kata4 version 2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,6 +1,5 @@
 
 package kata4;
-
 import java.io.FileNotFoundException;
 import java.util.List;
 import model.Histogram;
@@ -11,14 +10,31 @@ import view.MailListReader;
 import static view.MailListReader.read;
 
 public class Kata4 {
-
+    List<Mail> mailList;
+    Histogram<String> histogram = new Histogram<String>();
     public static void main(String[] args) throws FileNotFoundException {
+        Kata4 aplicacion = new Kata4();
+        aplicacion.execute();
+        }
+    
+    public void execute() throws FileNotFoundException {
+        input();
+        process();
+        output();
+    }
+    
+    public void input() throws FileNotFoundException {
         String fileName = new String("C:\\Users\\Lab-DIS\\Documents\\NetBeansProjects\\kata4\\email.txt");
-        List<Mail> mailList;
+        Mail email = new Mail(fileName);
         mailList = read(fileName);
-        Histogram<String> histogram = new Histogram<String>();
+    }
+    
+    public void process() {
         histogram = build(mailList);
+    }
+    
+    public void output() {
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
-    }
+}
